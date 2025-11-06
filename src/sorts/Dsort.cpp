6eb8c590f63faa5fa8ac0ded7eafc5c2d6d3b6e0
eatonjeremy <eatonjer04@gmail.com>
2025-11-06 20:11:18 +0000

    Work on MergeSort

--- src/sorts/Dsort.cpp
+++ src/sorts/Dsort.cpp
@@ -10,4 +10,16 @@
 
 // Your DSort function(s) goes here ..
 
+long* mergeSort(long data[], int size, int start_point) {
+	if(size !=1){
+		int left_size = ((size/2)+(size%2))
+		int right_size = (size/2)
+		mergeSort(data[], left_size , start_point);
+		mergeSort(data[], right_size , start_point + left_side);
+	}
 
+}
+
+long* merge(long array1[], long array2[], long array3[], int left_size, int right_size){
+
+}
