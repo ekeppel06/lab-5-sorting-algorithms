b4fb7d0df479e85a3004e8d7366d864aab4848d2
fbrzozow <fbrzozow@villanova.edu>
2025-11-09 19:04:15 +0000

    Final Selection Sort

--- src/sorts/Csort.cpp
+++ src/sorts/Csort.cpp
@@ -9,11 +9,15 @@
 // Your CSort function(s) goes here ..
 // 6, 76, 42, 10, 555, 30, 12, 0, -1, 4
 void SelectionSort(long a[], int size){
-	for(int i = 0, least; i < size; i++){
-		least = i;
-		for(int j = 1; j < i; j++){
-			swap(a[j-1], a[least-1]);
-
+	for(int i = 1, j, least; i < size ; i++){
+		for(int j = 0, least = i; j < i; j++){
+			// a[j] is first
+			if(a[least] < a[j]){
+				least = j;
+				swap(a[least], a[i]);
+			}
 		}
 	}
 }
+
+
